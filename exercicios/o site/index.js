995ae84b68e3d303b8dcc7f8b4eb0c14112e4dd0
Fabricio martins dos santos <fabricio200413@gmail.com>
2022-10-26 11:02:37 -0300

    atualização

--- exercicios/o site/index.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('botao-alt')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
